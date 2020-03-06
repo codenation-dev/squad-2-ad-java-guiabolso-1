a0fe901693e9e162797fc85486a2d29aea53a940
Ca-uê <59262999+ca-ue@users.noreply.github.com>
2020-03-06 13:08:14 -0300

    Add files via upload

---
+++ CentralDeErros/src/main/java/com/squad2/CentralDeErros/CentralDeErrosApplication.java
@@ -0,0 +1,13 @@
+package com.squad2.CentralDeErros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeErrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeErrosApplication.class, args);
+	}
+
+}
---
+++ CentralDeErros/src/test/java/com/squad2/CentralDeErros/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad2.CentralDeErros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
