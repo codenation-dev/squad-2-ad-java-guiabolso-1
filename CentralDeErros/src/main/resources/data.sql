12146aa81865f84b4928e4b1d5dd77d64d93290d
Renata Abreu <renata89abreu@gmail.com>
2020-03-14 00:21:29 -0300

    new lines to Log

--- CentralDeErros/src/main/resources/data.sql
+++ CentralDeErros/src/main/resources/data.sql
@@ -5,22 +5,42 @@ INSERT INTO USUARIO VALUES (3, 'Caue');
 INSERT INTO USUARIO VALUES (4, 'Jaqueline');
 INSERT INTO USUARIO VALUES (5, 'Luana');
 
-INSERT INTO LOG VALUES (1, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 1);
-INSERT INTO LOG VALUES (2, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 1);
-INSERT INTO LOG VALUES (3, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 1);
+INSERT INTO LOG VALUES (1, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 0, 1);
+INSERT INTO LOG VALUES (2, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 0, 1);
+INSERT INTO LOG VALUES (3, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 0, 1);
 
-INSERT INTO LOG VALUES (4, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 2);
-INSERT INTO LOG VALUES (5, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 2);
-INSERT INTO LOG VALUES (6, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 2);
+INSERT INTO LOG VALUES (4, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 0, 2);
+INSERT INTO LOG VALUES (5, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 0, 2);
+INSERT INTO LOG VALUES (6, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 0, 2);
 
-INSERT INTO LOG VALUES (7, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 3);
-INSERT INTO LOG VALUES (8, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 3);
-INSERT INTO LOG VALUES (9, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 3);
+INSERT INTO LOG VALUES (7, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 0, 3);
+INSERT INTO LOG VALUES (8, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 0, 3);
+INSERT INTO LOG VALUES (9, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 0, 3);
 
-INSERT INTO LOG VALUES (10, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 4);
-INSERT INTO LOG VALUES (11, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 4);
-INSERT INTO LOG VALUES (12, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 4);
+INSERT INTO LOG VALUES (10, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 0, 4);
+INSERT INTO LOG VALUES (11, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 0, 4);
+INSERT INTO LOG VALUES (12, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 0, 4);
 
-INSERT INTO LOG VALUES (13, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 5);
-INSERT INTO LOG VALUES (14, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 5);
-INSERT INTO LOG VALUES (15, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 5);
+INSERT INTO LOG VALUES (13, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 0, 5);
+INSERT INTO LOG VALUES (14, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 0, 5);
+INSERT INTO LOG VALUES (15, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 0, 5);
+
+INSERT INTO LOG VALUES (16, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 1);
+INSERT INTO LOG VALUES (17, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 1);
+INSERT INTO LOG VALUES (18, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 1);
+
+INSERT INTO LOG VALUES (19, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 2);
+INSERT INTO LOG VALUES (20, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 2);
+INSERT INTO LOG VALUES (21, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 2);
+
+INSERT INTO LOG VALUES (22, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 3);
+INSERT INTO LOG VALUES (23, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 3);
+INSERT INTO LOG VALUES (24, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 3);
+
+INSERT INTO LOG VALUES (25, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 4);
+INSERT INTO LOG VALUES (26, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 4);
+INSERT INTO LOG VALUES (27, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 4);
+
+INSERT INTO LOG VALUES (28, 0, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 0, 'Origem', 1, 5);
+INSERT INTO LOG VALUES (29, 1, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 1, 'Origem', 1, 5);
+INSERT INTO LOG VALUES (30, 2, CURRENT_TIMESTAMP(), 'Descricao', 'Log_Evento', 2, 'Origem', 1, 5);
