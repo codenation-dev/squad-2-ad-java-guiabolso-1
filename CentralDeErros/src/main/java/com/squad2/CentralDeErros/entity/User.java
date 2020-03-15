510e82cc6651970bd0ba1c2e22ccf75185ee722f
Renata Abreu <renata89abreu@gmail.com>
2020-03-15 19:48:26 -0300

    getters and setters generated for User.java

--- CentralDeErros/src/main/java/com/squad2/CentralDeErros/entity/User.java
+++ CentralDeErros/src/main/java/com/squad2/CentralDeErros/entity/User.java
@@ -14,4 +14,19 @@ public class User {
     @NotNull
     private String name;
 
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
