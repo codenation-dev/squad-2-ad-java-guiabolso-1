28a4a7dbb488f8affe9c1498e5d0b9501e87adcc
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-03-18 10:22:11 -0300

    improving signup page

--- Front-End/central-erros/src/pages/SignUp/index.js
+++ Front-End/central-erros/src/pages/SignUp/index.js
@@ -4,28 +4,34 @@ import FormSignUp from '../../components/FormSignUp'
 
 import './styles.css'
 
-const Signup =() =>{
-    const [name , setName]=useState('')
-    const[email, setEmail]=useState('')
-    const [password, setPassword]=useState('')
+const Signup = () => {
+    const [name, setName] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    let history = useHistory()
 
-    const sent = () =>{
-        alert("Cadastro efetuado com sucesso! Efetue o login!")
- 
+    const sent = (e) => {
+        e.preventDefault()
+        if (name && email && password) {
+            alert("Cadastro efetuado com sucesso! Efetue o login!")
+            history.push("/sign-in")
+        } else {
+            alert("Preencha todos os campos.")
+        }
     }
 
-    return(
-        <div className ='containerSignUp'>
+    return (
+        <div className='containerSignUp'>
             <FormSignUp
-            title ='Cadastro'
-            submit='Enviar!'
-            onChangeName = {(e) => setName(e.target.value)}
-            onChangeEmail={(e) => setEmail(e.target.value)}
-            onChangePassword={(e) => setPassword(e.target.value)}
-            onSubmit={() => {sent()}}
+                title='Cadastro'
+                submit='Enviar!'
+                onChangeName={(e) => setName(e.target.value)}
+                onChangeEmail={(e) => setEmail(e.target.value)}
+                onChangePassword={(e) => setPassword(e.target.value)}
+                onSubmit={(e) => { sent(e) }}
             />
 
-          
+
 
         </div>
     )
