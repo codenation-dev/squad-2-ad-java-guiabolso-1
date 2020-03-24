090d62de990ca58dd240a79d81fcfeae27388075
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-03-24 18:06:38 -0300

    integrating routes and api

--- Front-End/central-erros/src/routes.js
+++ Front-End/central-erros/src/routes.js
@@ -40,7 +40,7 @@ function Routes() {
                     <Route path="/sign-in" component={Login} />
                     <Route path="/sign-up" component={SignUp} />
                     <PrivateRoute path="/home" component={Home} />
-                    <Route path="/forgot-password" component={ForgotPassword} />
+                    <Route path="/forgotPassword" component={ForgotPassword} />
                     <Route path="*" component={NotFound} />
                 </Switch>
             </BrowserRouter>
