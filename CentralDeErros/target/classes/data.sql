1522310c127975713df7896a304554a1d200f6b5
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-03-24 13:02:35 -0300

    Merge branch 'master' of https://github.com/codenation-dev/squad-2-ad-java-guiabolso-1

---
+++ CentralDeErros/target/classes/data.sql
@@ -0,0 +1,46 @@
+/* DADOS INICIAIS PARA TESTE */
+INSERT INTO USER VALUES (1, 'Renata');
+INSERT INTO USER VALUES (2, 'Grace');
+INSERT INTO USER VALUES (3, 'Caue');
+INSERT INTO USER VALUES (4, 'Jaqueline');
+INSERT INTO USER VALUES (5, 'Luana');
+
+INSERT INTO LOG VALUES (1, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 0, 1);
+INSERT INTO LOG VALUES (2, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 0, 1);
+INSERT INTO LOG VALUES (3, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 0, 1);
+
+INSERT INTO LOG VALUES (4, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 0, 2);
+INSERT INTO LOG VALUES (5, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 0, 2);
+INSERT INTO LOG VALUES (6, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 0, 2);
+
+INSERT INTO LOG VALUES (7, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 0, 3);
+INSERT INTO LOG VALUES (8, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 0, 3);
+INSERT INTO LOG VALUES (9, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 0, 3);
+
+INSERT INTO LOG VALUES (10, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 0, 4);
+INSERT INTO LOG VALUES (11, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 0, 4);
+INSERT INTO LOG VALUES (12, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 0, 4);
+
+INSERT INTO LOG VALUES (13, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 0, 5);
+INSERT INTO LOG VALUES (14, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 0, 5);
+INSERT INTO LOG VALUES (15, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 0, 5);
+
+INSERT INTO LOG VALUES (16, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 1, 1);
+INSERT INTO LOG VALUES (17, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 1, 1);
+INSERT INTO LOG VALUES (18, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 1, 1);
+
+INSERT INTO LOG VALUES (19, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 1, 2);
+INSERT INTO LOG VALUES (20, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 1, 2);
+INSERT INTO LOG VALUES (21, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 1, 2);
+
+INSERT INTO LOG VALUES (22, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 1, 3);
+INSERT INTO LOG VALUES (23, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 1, 3);
+INSERT INTO LOG VALUES (24, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 1, 3);
+
+INSERT INTO LOG VALUES (25, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 1, 4);
+INSERT INTO LOG VALUES (26, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 1, 4);
+INSERT INTO LOG VALUES (27, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 1, 4);
+
+INSERT INTO LOG VALUES (28, CURRENT_TIMESTAMP(), 0, 'Description', 'Event Long', 0, 'Source', 1, 5);
+INSERT INTO LOG VALUES (29, CURRENT_TIMESTAMP(), 1, 'Description', 'Event Long', 1, 'Source', 1, 5);
+INSERT INTO LOG VALUES (30, CURRENT_TIMESTAMP(), 2, 'Description', 'Event Long', 2, 'Source', 1, 5);
