f68286a83861be7d5a17372f76f658dbf1fc49c7
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-03-28 14:46:49 -0300

    remove front-end from repository

--- Front-End/central-erros/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = (props) => {
-    return(
-        <span className={props.classTitle}>{props.title}</span>
-    )
-}
-
-export default Header
--- Front-End/central-erros/src/components/Link/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Link = (props) => {
-    return(
-        <span className={props.classLink}>{props.firstText}<a href={props.href}>{props.secondText}</a></span>
-    )
-}
-
-export default Link
