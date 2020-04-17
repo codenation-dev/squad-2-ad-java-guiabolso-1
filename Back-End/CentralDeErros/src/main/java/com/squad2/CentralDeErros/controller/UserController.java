a218d794e6ade56372746bbab77fb4c15f8e1593
grcferrari <grc.ferrari@gmail.com>
2020-04-17 18:10:04 -0300

    fixing bugs on data.sql

--- Back-End/CentralDeErros/src/main/java/com/squad2/CentralDeErros/controller/UserController.java
+++ Back-End/CentralDeErros/src/main/java/com/squad2/CentralDeErros/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
